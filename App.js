c41ecb3b528076cb909216caa6ea2046769bc446
Dev Huy <devhuy1011@gmail.com>
2021-12-08 01:03:35 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView
+      style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+      <Text style={{fontSize: 20}}>Hello World! 🚀 🥳</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
